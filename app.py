7de9ee8237c72f80400440e5dcbe7b637ea78f23
adil-km <adilkmparappur@gmail.com>
2025-08-17 12:13:24 +0530

    implemented basic CRUD operations

--- app.py
+++ app.py
@@ -1,18 +1,57 @@
+from datetime import datetime
 from flask import Flask, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(24)
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+# database model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable = False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route("/")
+    def __repr__(self):
+        return f"{self.id}"
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        content = request.form['content']
+        try:
+            task = Task(task=content)
+            db.session.add(task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"Error: {e}"
 
+    else:
+        contents = db.session.query(Task.task, Task.id).order_by(Task.created_at.desc()).all()
+        return render_template("index.html",contents=contents)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task = db.get_or_404(Task, id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/edit/<int:id>", methods=["GET","POST"])
+def edit(id):
+    task = db.get_or_404(Task, id)
+    db.session.delete(task)
+    db.session.commit()
+    return render_template("edit.html",content=task)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
