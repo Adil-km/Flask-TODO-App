8dac89531f28e1139c18d4ed4944a1e00862d254
adil-km <adilkmparappur@gmail.com>
2025-08-17 14:40:10 +0530

    deployment fix 1

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from datetime import datetime
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-
 app.secret_key = os.urandom(24)
+
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-# database model
+
+# Database model
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(100), nullable = False)
+    task = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"{self.id}"
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# Routes
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
@@ -31,10 +36,9 @@ def index():
             return redirect("/")
         except Exception as e:
             return f"Error: {e}"
-
     else:
         contents = Task.query.order_by(Task.created_at.desc()).all()
-        return render_template("index.html",contents=contents)
+        return render_template("index.html", contents=contents)
 
 @app.route("/delete/<int:id>")
 def delete(id):
@@ -42,7 +46,6 @@ def delete(id):
     db.session.delete(task)
     db.session.commit()
     return redirect("/")
-
 
 @app.route("/edit/<int:id>", methods=["GET","POST"])
 def edit(id):
@@ -52,9 +55,7 @@ def edit(id):
         db.session.commit()
         return redirect("/")
     else:
-        return render_template('edit.html', content = task)
+        return render_template('edit.html', content=task)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run()
